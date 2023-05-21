9a6fc298c4ede9c245c10d05e98f810f2205e79f
sashadubovyi <125696737+sashadubovyi@users.noreply.github.com>
2023-05-21 17:08:08 +0300

    done

---
+++ index.js
@@ -0,0 +1,5 @@
+var lightbox = new SimpleLightbox(".first a", { captionDelay: 250 });
+var lightbox = new SimpleLightbox(".second a", { captionDelay: 250 });
+var lightbox = new SimpleLightbox(".third a", { captionDelay: 250 });
+
+// scroll
